4c13c2507c2f59b9ee8eefe1012c24f284fb5b16
Reiss1x <gabrielreis.reis1903@gmail.com>
2024-02-20 15:15:12 -0300

    feat: initial config

---
+++ src/main/java/com/uolhost/uolhostbackendchallenge/UolhostBackendChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.uolhost.uolhostbackendchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UolhostBackendChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UolhostBackendChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uolhost/uolhostbackendchallenge/UolhostBackendChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uolhost.uolhostbackendchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UolhostBackendChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
