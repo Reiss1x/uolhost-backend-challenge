7d61e7e5737d40856900033de9317b0a7961d8ce
Reiss1x <gabrielreis.reis1903@gmail.com>
2024-02-21 16:50:57 -0300

    feat: endpoints for player creation

--- src/main/java/com/uolhost/uolhostbackendchallenge/Controllers/PlayerController.java
+++ src/main/java/com/uolhost/uolhostbackendchallenge/Controllers/PlayerController.java
@@ -1,18 +1,36 @@
 package com.uolhost.uolhostbackendchallenge.Controllers;
 
 import com.uolhost.uolhostbackendchallenge.Service.PlayerService;
+import com.uolhost.uolhostbackendchallenge.domain.Player;
+import com.uolhost.uolhostbackendchallenge.domain.Team;
+import com.uolhost.uolhostbackendchallenge.dtos.PlayerDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import java.util.List;
+
+@RestController("/players")
 public class PlayerController {
 
     @Autowired
     private PlayerService ps;
 
-    @GetMapping("/vingadores")
-    public void vingadores() throws Exception {
-        ps.validatePlayer();
+    @PostMapping
+    public ResponseEntity<Player> registerPlayer(@RequestBody PlayerDTO player) throws Exception {
+        Player newPlayer = ps.validatePlayer(player);
+        if(newPlayer != null){
+            return new ResponseEntity<>(newPlayer, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(null, (HttpStatus.FORBIDDEN));
+        }
+    }
+    @GetMapping
+    public ResponseEntity<List<Player>> getPlayers(){
+        return new ResponseEntity<List<Player>>(ps.getPlayers(), HttpStatus.OK);
     }
 }
