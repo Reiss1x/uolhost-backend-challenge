d77c13c9a50053804810c228cd7f4b2f81fe725b
Reiss1x <gabrielreis.reis1903@gmail.com>
2024-02-23 16:39:00 -0300

    fix: small fixes and end of challenge

--- src/main/java/com/uolhost/uolhostbackendchallenge/controllers/PageController.java
+++ src/main/java/com/uolhost/uolhostbackendchallenge/controllers/PageController.java
@@ -1,11 +1,9 @@
 package com.uolhost.uolhostbackendchallenge.controllers;
 
-import com.uolhost.uolhostbackendchallenge.domain.Player;
+
 import com.uolhost.uolhostbackendchallenge.service.PlayerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -17,14 +15,10 @@ public class PageController {
     @Autowired
     private PlayerService ps;
 
-    @RequestMapping("/homes")
+    @RequestMapping("/")
     public ModelAndView home(){
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("home.html");
         return modelAndView;
-    }
-    @GetMapping("/")
-    public ResponseEntity<List<Player>> getPlayers(){
-        return new ResponseEntity<List<Player>>(ps.getPlayers(), HttpStatus.OK);
     }
 }
