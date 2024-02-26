e7ed2909462697525f7cf6d6ad4b05dd5d32c865
Reiss1x <gabrielreis.reis1903@gmail.com>
2024-02-26 16:22:02 -0300

    fix: fixes for endpoint mapping

--- src/main/java/com/uolhost/uolhostbackendchallenge/controllers/PageController.java
+++ src/main/java/com/uolhost/uolhostbackendchallenge/controllers/PageController.java
@@ -4,6 +4,7 @@ package com.uolhost.uolhostbackendchallenge.controllers;
 import com.uolhost.uolhostbackendchallenge.service.PlayerService;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -15,7 +16,7 @@ public class PageController {
     @Autowired
     private PlayerService ps;
 
-    @RequestMapping("/")
+    @GetMapping("/home")
     public ModelAndView home(){
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("home.html");
